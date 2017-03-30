ed1f5ee82497eaba0d89973720909fd7fa6eb9c6
Ivan Trifonov <trivan@gojuno.com>
2017-03-30 21:13:25 +0200

    added URL

--- index.js
+++ index.js
@@ -5,7 +5,20 @@ fs = require('fs');
 
 app.use(cors());
 
+app.get('/url', function(req, res) {
+	res.send("<a href=\"http://jit-im-upc-de.upc.biz/content?AccountKey=547131&uniqueTokenKey=HIHQHROS&type=21\">link</a>");
+});
+
 app.get('/peal/api/sso/users', function (req, res) {
+	fs.readFile('./pealssousers.json', 'utf8', function (err,data) {
+  		if (err) {
+    		return console.log(err);
+  		}
+  		res.status(200).send(data)	
+	});
+});
+
+app.get('/peal/api/sso/users/resendverificationlink', function (req, res) {
 	fs.readFile('./pealssousers.json', 'utf8', function (err,data) {
   		if (err) {
     		return console.log(err);

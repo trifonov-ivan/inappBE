b1b3e199215c498b8a8f8d2ea8d81908f534d022
Ivan Trifonov <trivan@gojuno.com>
2017-03-30 23:13:27 +0200

    emails

--- index.js
+++ index.js
@@ -21,6 +21,29 @@ var mailOptions = {
 
 app.use(cors());
 
+app.get('/mail', function(req, res){
+		fs.readFile('./forgot.html', 'utf8', function (err,data) {
+  			if (err) {
+    			return console.log(err);
+  			}
+
+	  		var mailOptions = {
+			    from: 'Unitymedia <service@email.unitymedia.de>', // sender address
+	    		to: 'iaf.epam@gmail.com', // list of receivers
+	    		subject: 'Bestätigung Ihrer E-Mail-Adresse', // Subject line
+	    		html: data
+			};
+
+
+	  		transporter.sendMail(mailOptions, (error, info) => {
+	    		if (error) {
+	        		return console.log(error);
+	    		}
+	    		console.log('Message %s sent: %s', info.messageId, info.response);
+			});
+	  	});
+	});
+
 app.get('/url', function(req, res) {
 	res.send("<a href=\"http://jit-im-upc-de.upc.biz/content?AccountKey=547131&uniqueTokenKey=HIHQHROS&type=21\">link</a>");
 });
@@ -36,16 +59,30 @@ app.get('/peal/api/sso/users', function (req, res) {
 
 app.post('/peal/api/sso/users/resendverificationlink', function (req, res) {
 	fs.readFile('./resendverificationlink.json', 'utf8', function (err,data) {
-		transporter.sendMail(mailOptions, (error, info) => {
-    		if (error) {
-        		return console.log(error);
-    		}
-    		console.log('Message %s sent: %s', info.messageId, info.response);
-		});
   		if (err) {
     		return console.log(err);
   		}
-  		res.status(200).send(data)	
+		fs.readFile('./forgot.html', 'utf8', function (err,data) {
+  			if (err) {
+    			return console.log(err);
+  			}
+
+	  		var mailOptions = {
+			    from: 'Unitymedia <service@email.unitymedia.de>', // sender address
+	    		to: 'iaf.epam@gmail.com', // list of receivers
+	    		subject: 'Bestätigung Ihrer E-Mail-Adresse', // Subject line
+	    		html: data
+			};
+
+
+	  		transporter.sendMail(mailOptions, (error, info) => {
+	    		if (error) {
+	        		return console.log(error);
+	    		}
+	    		console.log('Message %s sent: %s', info.messageId, info.response);
+			});
+	  		res.status(200).send(updateData)	
+	  	});
 	});
 });
 
